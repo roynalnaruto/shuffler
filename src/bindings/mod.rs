bac4eb32ff3b1fa86032eaa4257872dcb130d3b9
Rohit Narurkar <rohit.narurkar@protonmail.com>
2020-07-31 00:55:02 +0530

    stream event filters from beacon contract

---
+++ src/bindings/mod.rs
@@ -0,0 +1,5 @@
+mod beacon_contract;
+pub use beacon_contract::BeaconContract;
+
+mod ierc20;
+pub use ierc20::IERC20;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,39 @@
+use anyhow::anyhow;
 use ethers::prelude::*;
+use std::{env, str::FromStr, time::Duration};
+
+mod bindings;
+use bindings::BeaconContract;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let (url, private_key) = env_var()?;
+    let ws = Ws::connect(url).await?;
+    let provider = Provider::new(ws).interval(Duration::from_secs(5));
+    let client = private_key.parse::<Wallet>()?.connect(provider);
+
+    let address = Address::from_str("79474439753C7c70011C3b00e06e559378bAD040")?;
+    let beacon_contract = BeaconContract::new(address, client);
+
+    let mut stream = beacon_contract.log_new_randomness_filter().stream().await?;
+
+    while let Some(log) = stream.next().await {
+        dbg!(log);
+    }
+
     Ok(())
+}
+
+fn env_var() -> anyhow::Result<(String, String)> {
+    let url = match env::var("PROVIDER_WSS_URL") {
+        Ok(url) => url,
+        Err(e) => return Err(anyhow!("Missing env var: {}", e)),
+    };
+
+    let private_key = match env::var("PRIVATE_KEY") {
+        Ok(pk) => pk,
+        Err(e) => return Err(anyhow!("Missing env var: {}", e)),
+    };
+
+    Ok((url, private_key))
 }
